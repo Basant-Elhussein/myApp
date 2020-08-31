839da84d314e13c329de71b8d10c89d75b9e190f
Basant-Elhussein <basantelhussein@aucegypt.edu>
2020-08-31 05:06:30 +0200

    initial setup

---
+++ app.js
@@ -0,0 +1,34 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const defRoutes = require('./routes/index');
+
+const app = express();
+
+/*Set View Engine*/
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+/*Setting Mongodb */
+mongoose.connect(process.env.database_url, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.log(error);
+});
+db.once('open', () => {
+  console.log('connected to Mongoose');
+});
+
+/*Routes*/
+app.use('/', defRoutes);
+
+app.listen(process.env.port || 3000, () => {
+  console.log('App is linstening on port 3000');
+});
