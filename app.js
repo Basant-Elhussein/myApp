99ff7db800ff04d199965cf361c3accc9a0e87a3
Basant-Elhussein <basantelhussein@aucegypt.edu>
2020-09-01 01:10:25 +0200

    edit

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ db.once('open', () => {
 /*Routes*/
 app.use('/', defRoutes);
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App is linstening on port 3000');
 });
